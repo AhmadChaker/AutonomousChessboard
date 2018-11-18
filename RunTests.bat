81302ec9e3d5a94c1b0e7fd56eb5d7e12050bcef
Ahmad Chaker <ahmad.t.chaker@gmail.com>
2018-11-18 20:13:04 +0000

    Script file to run unit tests from PyCharm

---
+++ RunTests.bat
@@ -0,0 +1,4 @@
+python -m unittest discover -s C:/Users/ahmad/Documents/Chess/Test/TestBoard -t C:\Users\ahmad\Documents\Chess\Test\TestBoard
+python -m unittest discover -s C:/Users/ahmad/Documents/Chess/Test/TestMiscellaneous -t C:\Users\ahmad\Documents\Chess\Test\TestMiscellaneous
+python -m unittest discover -s C:/Users/ahmad/Documents/Chess/Test/TestPieces -t C:\Users\ahmad\Documents\Chess\Test\TestPieces
+python -m unittest discover -s C:/Users/ahmad/Documents/Chess/Test/TestUtilities -t C:\Users\ahmad\Documents\Chess\Test\TestUtilities
