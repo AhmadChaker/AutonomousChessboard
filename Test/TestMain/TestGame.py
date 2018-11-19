a45b16b4cf903f86415ff2526054e664262848e1
Ahmad Chaker <ahmad.t.chaker@gmail.com>
2018-11-19 23:49:15 +0000

    Implement game unit tests

--- Test/TestMain/TestGame.py
+++ Test/TestMain/TestGame.py
@@ -21,15 +21,62 @@ class TestGame(unittest.TestCase):
         chessBoard = ChessBoard()
         history = History()
         self.Game = Game(history, chessBoard)
+        MoveHelpers.Update(history)
 
     def tearDown(self):
         self.Game = None
+        MoveHelpers.Update(None)
 
 # region CanMove tests
 
-    def test_CanMove_InvalidFromCoordinate_Returns(self):
-        initialTeam = TeamEnum.NoTeam
-        expectedTeam = TeamEnum.NoTeam
-        self.assertEqual(BoardHelpers.GetOpposingTeam(initialTeam), expectedTeam)
+    def test_CanMove_HasGameEndedSetToTrue_ReturnsFalse(self):
+        fromCoord = "C2"
+        toCoord = "C4"
+
+        self.Game.SetHasGameEnded(True)
+        self.assertFalse(self.Game.CanMove(fromCoord, toCoord).IsSuccessful())
+
+    def test_CanMove_FromCoordIsInvalid_ReturnsFalse(self):
+        fromCoord = "00"
+        toCoord = "C4"
+
+        self.assertFalse(self.Game.CanMove(fromCoord, toCoord).IsSuccessful())
+
+    def test_CanMove_ToCoordIsInvalid_ReturnsFalse(self):
+        fromCoord = "C2"
+        toCoord = "Z1"
+
+        self.assertFalse(self.Game.CanMove(fromCoord, toCoord).IsSuccessful())
+
+    def test_CanMove_PieceBeingMovedIsNoPiece_ReturnsFalse(self):
+        fromCoord = "C4"
+        toCoord = "C5"
+
+        self.assertFalse(self.Game.CanMove(fromCoord, toCoord).IsSuccessful())
+
+    def test_CanMove_NotThisPlayersTurn_ReturnsFalse(self):
+        fromCoord = "C7"
+        toCoord = "C6"
+
+        self.assertFalse(self.Game.CanMove(fromCoord, toCoord).IsSuccessful())
+
+    def test_CanMove_PieceCantMoveToThisLocation_ReturnsFalse(self):
+        fromCoord = "C2"
+        toCoord = "C5"
+
+        self.assertFalse(self.Game.CanMove(fromCoord, toCoord).IsSuccessful())
+
+    def test_CanMove_ValidMove_ReturnsTrue(self):
+        fromCoord = "C2"
+        toCoord = "C4"
+
+        self.assertTrue(self.Game.CanMove(fromCoord, toCoord).IsSuccessful())
+
+# endregion
+
+# region PerformPawnPromotionCheck tests
+
+    def test_PerformPawnPromotionCheck_PawnMoving_YCoordIsZero(self):
+        pass
 
 # endregion
