5a8e9f28bcfaca56ac7ce305816ddf749ade5ae8
Ahmad Chaker <ahmad.t.chaker@gmail.com>
2019-01-29 15:50:52 +1100

    Add skill levels

--- Uci/Levels.py
+++ Uci/Levels.py
@@ -39,20 +39,21 @@ class Level:
     def GetThinkingTime(self):
         return self.__thinkingTime
 
+
 # TODO Fix levels
 # Levels of AI
-LevelsList = [Level(LevelEnums.ONE, 10, 10, 200),
-              Level(LevelEnums.TWO, 3, 5, 100),
-              Level(LevelEnums.THREE, 3, 5, 100),
-              Level(LevelEnums.FOUR, 3, 5, 100),
-              Level(LevelEnums.FIVE, 3, 5, 100),
-              Level(LevelEnums.SIX, 3, 5, 100),
-              Level(LevelEnums.SEVEN, 3, 5, 100),
-              Level(LevelEnums.EIGHT, 3, 5, 100),
-              Level(LevelEnums.NINE, 3, 5, 100),
-              Level(LevelEnums.TEN, 3, 5, 100),
-              Level(LevelEnums.ELEVEN, 3, 5, 100),
-              Level(LevelEnums.TWELVE, 3, 5, 100),
-              Level(LevelEnums.THIRTEEN, 3, 5, 100),
-              Level(LevelEnums.FOURTEEN, 3, 5, 100),
-              Level(LevelEnums.FIFTEEN, 3, 5, 100)]
+LevelsList = [Level(LevelEnums.ONE, 3, 1, 50),
+              Level(LevelEnums.TWO, 5, 2, 100),
+              Level(LevelEnums.THREE, 6, 3, 150),
+              Level(LevelEnums.FOUR, 7, 3, 200),
+              Level(LevelEnums.FIVE, 9, 3, 200),
+              Level(LevelEnums.SIX, 11, 4, 200),
+              Level(LevelEnums.SEVEN, 12, 5, 200),
+              Level(LevelEnums.EIGHT, 13, 6, 300),
+              Level(LevelEnums.NINE, 14, 7, 300),
+              Level(LevelEnums.TEN, 15, 8, 300),
+              Level(LevelEnums.ELEVEN, 16, 8, 300),
+              Level(LevelEnums.TWELVE, 17, 9, 300),
+              Level(LevelEnums.THIRTEEN, 18, 10, 300),
+              Level(LevelEnums.FOURTEEN, 19, 11, 400),
+              Level(LevelEnums.FIFTEEN, 20, 12, 500)]
