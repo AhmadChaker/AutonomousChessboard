3fde14ec918dcc7b0b3cd741f0812eda0bb25a88
Ahmad Chaker <ahmad.t.chaker@gmail.com>
2019-02-01 14:12:50 +1100

    1) Implement more intuitive and readable function names 2) Add clarification comments to dummy code

--- Main/main.py
+++ Main/main.py
@@ -15,9 +15,14 @@ from Utilities.OSConfiguration import OSConfiguration
 from Miscellaneous.Constants import PlayerEnum
 
 
-def StartFunction():
-    # Game type?  Human vs AI, AI vs AI, Human vs Human
+# region GUI element handlers
+
+def StartClicked():
+
+    # Game type  Human vs AI, AI vs AI, Human vs Human
     # In case of Human vs AI - Get color we want to start with
+
+    # TODO: These properties will be configurable via user selection, hard-code these for testing right now
     gameType = Miscellaneous.Constants.GameType.AIvsAI
     humanColour = Miscellaneous.Constants.TeamEnum.White
     aiLevel = LevelsList[0]
@@ -29,30 +34,31 @@ def StartFunction():
     GameRequestQueue.put(BaseMessage(Actions.Configuration, GameConfigurationMessage(gameType, humanColour)))
 
 
-def ResetFunction():
+def ResetClicked():
     # TODO figure out how to purge queues
     EngineRequestQueue.put(BaseMessage(Actions.Reset, None))
     GameRequestQueue.put(BaseMessage(Actions.Reset, None))
 
 
-def ClickedFunction():
-    logging.error("ClickedButton pid: " + str(os.getpid()))
-    logging.error("Entered ClickedButton")
-
+def MoveClicked():
+    # This is dummy input code. TODO: perform proper validation when audio inputter is working correctly
     moveCoords = MoveTextBox.value
     fromCoord = moveCoords[0] + moveCoords[1]
     toCoord = moveCoords[2] + moveCoords[3]
     GameRequestQueue.put(BaseMessage(Actions.Movement, GameMovementMessage(PlayerEnum.Human, fromCoord, toCoord)))
     MoveTextBox.clear()
 
+# endregion
 
-# Every new process should call this first to hookup logging
+
+# Must run this first to setup multi-process logging
 def StartLogProcess(logQueue):
     logListenerProcess = Process(target=LogListener.Listen, args=(logQueue,))
     logListenerProcess.daemon = True
     logListenerProcess.start()
 
 
+# Every new process should call this method first to hookup multi-process logging
 def ProcessStartupBaseTasks(logQueue):
     handler = logging.handlers.QueueHandler(logQueue)
     root = logging.getLogger()
@@ -81,6 +87,17 @@ def EngineRequestListenerRunner(logQueue, engineRequestListenerObj):
     engineRequestListenerObj.StartListeningForRequests()
 
 
+def DrawGUI():
+    # TODO: Implement actual proper test interaction interface instead of this dummy one
+    app = App(title="ChessBoard", width=260, height=60, layout="grid")
+    StartButton = PushButton(app, grid=[0, 0], text="Start", command=StartClicked)
+    MoveTextBlock = Text(app, text="Move", grid=[1,0], align="left")
+    MoveTextBox = TextBox(app, grid=[2, 0], align="left")
+    GoButton = PushButton(app, grid=[3,0], text="Go", command=MoveClicked)
+    ResetButton = PushButton(app, grid=[4, 0], text="Reset", command=ResetClicked)
+    app.display()
+
+
 if __name__ == '__main__':
     # Setup multi-process logging infrastructure
     LoggingQueue = Queue()
@@ -103,11 +120,4 @@ if __name__ == '__main__':
     # Run processes
     StartProcesses(LoggingQueue, GameRequestListenerObj, EngineRequestListenerObj)
 
-    logging.error("Inside main pid: " + str(os.getpid()))
-    app = App(title="Sheena", width=600, height=600, layout="grid")
-    StartButton = PushButton(app, grid=[0, 0], text="Start", command=StartFunction)
-    MoveTextBlock = Text(app, text="Move", grid=[1,0], align="left")
-    MoveTextBox = TextBox(app, grid=[2, 0], align="left")
-    GoButton = PushButton(app, grid=[3,0], text="Go", command=ClickedFunction)
-    ResetButton = PushButton(app, grid=[4, 0], text="Reset", command=ResetFunction)
-    app.display()
+    DrawGUI()
